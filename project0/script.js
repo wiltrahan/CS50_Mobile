904ccfcb5f5b054974c86f212239dc76dfd051eb
wiltrahan <twilorip@gmail.com>
2018-08-20 17:33:36 -0400

    all requirements complete

--- project0/script.js
+++ project0/script.js
@@ -3,7 +3,7 @@ const classNames = {
   TODO_CHECKBOX: 'todo-checkbox',
   TODO_TEXT: 'todo-text',
   TODO_DELETE: 'todo-delete',
-}
+};
 
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
@@ -18,20 +18,22 @@ function newTodo() {
         alert("Please add something TODO!");
     } else {
         listItemId++;
-        addToView(userTodo.value, listItemId);
+        createAndAddToView(userTodo.value, listItemId);
     }
-}
+};
 
-function addToView(listItem, listItemId) {
-    const li = document.createElement("LI");
+function createAndAddToView(listItem, listItemId) {
+    const li = document.createElement("li");
+    const text = document.createTextNode(listItem);
     const checkbox = createCheckbox(listItemId);
 
+    li.appendChild(text);
     li.appendChild(checkbox);
-    li.appendChild(document.createTextNode(listItem));
+    li.className = classNames.TODO_ITEM + " " + classNames.TODO_TEXT;
     list.appendChild(li);
 
     addToListArrayAndIncrementCount(listItem, checkbox.id);
-}
+};
 
 function createCheckbox() {
     const checkbox = document.createElement("input");
@@ -39,14 +41,14 @@ function createCheckbox() {
         checkbox.id = listItemId - 1;
         checkbox.className = classNames.TODO_CHECKBOX;
         checkbox.addEventListener('change', function() {
-        if(this.checked) {
-            onCheckMarkDecrementCount();
-        } else if(this.checked === false) {
-            uncheckedIncrementCount();
-        }
+            if(this.checked) {
+                onCheckMarkDecrementCount();
+            } else if(this.checked === false) {
+                uncheckAndIncrementCount();
+            }
         });
     return checkbox;
-}
+};
 
 function addToListArrayAndIncrementCount(listItem, id) {
     const todoObject = {};
@@ -55,20 +57,21 @@ function addToListArrayAndIncrementCount(listItem, id) {
 
     arrayOfTodos.push(todoObject);
     itemCountSpan.innerText = arrayOfTodos.length;
-    let checked = document.querySelectorAll('input[type="checkbox"]:checked').length;
+    uncheckedItemCount();
 
+};
+
+function uncheckedItemCount() {
+    const checked = document.querySelectorAll('input[type="checkbox"]:checked').length;
     uncheckedCountSpan.innerText = arrayOfTodos.length - checked;
-
-    console.log(arrayOfTodos);
-}
-
+};
 
 function onCheckMarkDecrementCount() {
     if(uncheckedCountSpan.innerText > 0) {
         uncheckedCountSpan.innerText--;
     }
-}
+};
 
-function uncheckedIncrementCount() {
+function uncheckAndIncrementCount() {
     uncheckedCountSpan.innerText++;
-}
+};
